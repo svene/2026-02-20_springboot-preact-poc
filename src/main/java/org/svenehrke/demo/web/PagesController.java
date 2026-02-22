5a333417a6dcf9821d006b40570751feb851b234
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2026-02-22 09:22:22 +0100

    pass JSON string as param to JTE

--- src/main/java/org/svenehrke/demo/web/PagesController.java
+++ src/main/java/org/svenehrke/demo/web/PagesController.java
@@ -4,14 +4,19 @@ import de.tschuehly.spring.viewcomponent.jte.ViewContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import tools.jackson.databind.ObjectMapper;
 
 @Controller
 public class PagesController {
 
 	private final SimpleViewComponent simpleViewComponent;
-
-	public PagesController(SimpleViewComponent simpleViewComponent) {
+	private final ObjectMapper objectMapper;
+	public PagesController(
+		SimpleViewComponent simpleViewComponent,
+		ObjectMapper objectMapper
+	) {
 		this.simpleViewComponent = simpleViewComponent;
+		this.objectMapper = objectMapper;
 	}
 
 	@GetMapping("/")
@@ -22,6 +27,10 @@ public class PagesController {
 	@GetMapping("/ui/pages/page1")
 	public String page1(Model model) {
 		model.addAttribute("greetee", "You");
+		record JJJ(String message){};
+		model.addAttribute("jjj", objectMapper.writeValueAsString(
+			new JJJ("myJJJ")
+		));
 		return "pages/page1";
 	}
 
